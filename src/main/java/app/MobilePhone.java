aa35eee83a5a6ab083b5e1fdfe9d22eeb6391a5f
xan2301 <2silverghost2@gmail.com>
2022-08-23 23:16:19 +0200

    1.0.0.1

---
+++ src/main/java/app/MobilePhone.java
@@ -0,0 +1,41 @@
+package app;
+
+public class MobilePhone {
+
+    private String system;
+
+    private int resolution;
+
+    private int productionYear;
+
+    public MobilePhone(String system, int resolution, int productionYear) {
+        this.system = system;
+        this.resolution = resolution;
+        this.productionYear = productionYear;
+    }
+
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int resolution) {
+        this.resolution = resolution;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(int productionYear) {
+        this.productionYear = productionYear;
+    }
+}
